a0359c2fcdfcd7ac1c80b4009ccda2d1e290c3ab
Othmane <mahfoud.othmane97@gmail.com>
2019-03-08 14:28:21 +0100

    Add JWT to HTTP headers

--- frontend_kudos/src/services/api.js
+++ frontend_kudos/src/services/api.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
-export function apiCall(method, path, data){
+export function setTokenHeader(token) {
+  if(token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
+  else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
+}
+
+export function apiCall(method, path, data) {
   return new Promise((resolve, reject) => {
     return axios[method](path, data)
     .then(res => {

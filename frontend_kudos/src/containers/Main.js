49205d14cba1f537d5f9b29fe1e28752faa50705
Othmane <mahfoud.othmane97@gmail.com>
2019-03-08 11:50:31 +0100

    Build simple authentication form

--- frontend_kudos/src/containers/Main.js
+++ frontend_kudos/src/containers/Main.js
@@ -2,12 +2,32 @@ import React from "react";
 import {Switch, Route, withRouter, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import Homepage from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
 
 const Main = props => {
   return(
     <div className="container">
       <Switch>
-        <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route
+          exact path = "/"
+          render = {props => <Homepage {...props} />}
+        />
+        <Route
+          exact path="/signin"
+          render={props => {
+            return(
+              <AuthForm buttonText="Log in" heading="Welcome Back" {...props}/>
+            );
+          }}
+        />
+        <Route
+          exact path="/signup"
+          render={props => {
+            return(
+              <AuthForm buttonText="Sign up" heading="Join Kudos today!" {...props}/>
+            );
+          }}
+        />
       </Switch>
 
     </div>

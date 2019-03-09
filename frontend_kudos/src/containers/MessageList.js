046c889f13dbf189077604b3f0fc4bdd240b4f83
Othmane <mahfoud.othmane97@gmail.com>
2019-03-09 09:41:21 +0100

    Minor changes

--- frontend_kudos/src/containers/MessageList.js
+++ frontend_kudos/src/containers/MessageList.js
@@ -1,27 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchMessages } from "../store/actions/messages"
+import { fetchMessages, removeMessage } from "../store/actions/messages";
 import MessageItem from "../components/MessageItem";
 
 class MessageList extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchMessages();
   }
   render() {
-    const { messages } = this.props;
+    const { messages, removeMessage, currentUser } = this.props;
     let messageList = messages.map(m => (
       <MessageItem
-        key = {m._id}
-        date = {m.createAt}
-        text = {m.text}
-        username = {m.user.username}
-        profileImgUrl = {m.user.profileImageUrl}
+        key={m._id}
+        date={m.createAt}
+        text={m.text}
+        username={m.user.username}
+        profileImageUrl={m.user.profileImageUrl}
+        removeMessage={removeMessage.bind(this, m.user._id, m._id)}
+        isCorrectUser={currentUser === m.user._id}
       />
     ));
     return (
-      <div className = "row-col-sm-8">
-        <div className = "offset-1 col-sm-10">
-          <ul className = "list-group" id = "messages">
+      <div className="row col-sm-8">
+        <div className="offset-1 col-sm-10">
+          <ul className="list-group" id="messages">
             {messageList}
           </ul>
         </div>
@@ -30,10 +32,13 @@ class MessageList extends Component {
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    messages: state.messages
+    messages: state.messages,
+    currentUser: state.currentUser.user.id
   };
 }
 
-export default connect(mapStateToProps, {fetchMessages})(MessageList);
+export default connect(mapStateToProps, { fetchMessages, removeMessage })(
+  MessageList
+);
